5e38cce213a68119c60d4f546893e54f4b587a40
likhia <59862767+likhia@users.noreply.github.com>
2022-03-07 09:55:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,33 +3,38 @@ import os
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from model.train import train_model
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
 api = Api(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
+
 
 if not os.path.isfile('credit-card-fraud-model.model'):
     train_model()
 
 loaded_model = joblib.load('credit-card-fraud-model.model')
 
-class MakePrediction(Resource):
+class predict(Resource):
     @staticmethod
+    @metrics.counter('predict', 'Number of prediction',
+         labels={'result': lambda: request.predicted_class})
     def post():
         posted_data = request.get_json()
         transaction_info = posted_data['transaction_detail']
 
         prediction = loaded_model.predict([transaction_info])[0]
-
         if prediction == 0:
-            predicted_class = 'Normal Transaction 1'
+            request.predicted_class = 'normal'
         else:
-            predicted_class = 'Fraud Transaction'
+            request.predicted_class = 'fraud'
 
         return jsonify({
-            'Prediction': predicted_class
+            'Prediction': request.predicted_class
         })
 
-api.add_resource(MakePrediction, '/predict')
+api.add_resource(predict, '/predict')
 
 
 if __name__ == '__main__':
